10e45a0286f632928daf05aacb84c735170c9ff2
tianling <2507073658@qq.com>
2017-03-20 18:01:41 +0800

    http server

---
+++ httpServer/cmake-build-debug/CMakeFiles/httpServer.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/luoxinyu-xy/Documents/workspace/httpServer/main.cpp" "C:/Users/luoxinyu-xy/Documents/workspace/httpServer/cmake-build-debug/CMakeFiles/httpServer.dir/main.cpp.obj"
+  "C:/Users/luoxinyu-xy/Documents/workspace/httpServer/server.cpp" "C:/Users/luoxinyu-xy/Documents/workspace/httpServer/cmake-build-debug/CMakeFiles/httpServer.dir/server.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ httpServer/cmake-build-debug/CMakeFiles/httpServer.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/httpServer.dir/main.cpp.obj"
+  "CMakeFiles/httpServer.dir/server.cpp.obj"
+  "httpServer.pdb"
+  "httpServer.exe"
+  "httpServer.exe.manifest"
+  "libhttpServer.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/httpServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
